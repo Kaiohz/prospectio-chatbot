c68d1704fe0bfe5b60147eec1d1813c28d8d3011
Kaiohz <goncalves.yohan@gmail.com>
2024-11-23 00:56:36 +0100

    add Embeddings

--- llm_client/llm_client.py
+++ llm_client/llm_client.py
@@ -2,16 +2,27 @@ from typing import List
 import httpx
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
-from config import LLM_URL, LLM_KEY, MODEL
+from config import LLM_URL, LLM_KEY, MODEL_CHAT, MODEL_EMBEDDINGS
 
 class LLMProxyChatOpenAI(ChatOpenAI):
-    def __init__(self, streaming: str = False) -> None:
+    def __init__(self) -> None:
         super().__init__(
             http_client=httpx.Client(http2=True, verify=False),
             http_async_client=httpx.AsyncClient(http2=True, verify=False),
             openai_api_base=LLM_URL,
-            model=MODEL,
+            model=MODEL_CHAT,
             openai_api_key=LLM_KEY,
-            streaming=streaming,
+            streaming=False,
             callbacks=[],
+        )
+
+
+class LLMProxyOpenAIEmbeddings(OpenAIEmbeddings):
+    def __init__(self) -> None:
+        super().__init__(
+            http_client=httpx.Client(http2=True, verify=False),
+            http_async_client=httpx.AsyncClient(http2=True, verify=False),
+            openai_api_base=LLM_URL,
+            model=MODEL_EMBEDDINGS,
+            openai_api_key=LLM_KEY,
         )
