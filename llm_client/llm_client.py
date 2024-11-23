18cf50d378e41df2c6a4d0f75aaffa3542faded3
Kaiohz <goncalves.yohan@gmail.com>
2024-11-23 01:02:19 +0100

    fix init proxy

--- llm_client/llm_client.py
+++ llm_client/llm_client.py
@@ -5,14 +5,14 @@ from langchain_openai import ChatOpenAI
 from config import LLM_URL, LLM_KEY, MODEL_CHAT, MODEL_EMBEDDINGS
 
 class LLMProxyChatOpenAI(ChatOpenAI):
-    def __init__(self) -> None:
+    def __init__(self, streaming: str = False) -> None:
         super().__init__(
             http_client=httpx.Client(http2=True, verify=False),
             http_async_client=httpx.AsyncClient(http2=True, verify=False),
             openai_api_base=LLM_URL,
             model=MODEL_CHAT,
             openai_api_key=LLM_KEY,
-            streaming=False,
+            streaming=streaming,
             callbacks=[],
         )
 
