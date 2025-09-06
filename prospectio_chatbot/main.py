34c6d149cb8563d0e99b87ff52ac3249c9ef4091
Kaiohz <yohan.goncalves@cosigma.io>
2025-09-06 18:26:08 +0200

    feat: refactor Dockerfile and main application structure to integrate Chainlit with FastAPI

--- prospectio_chatbot/main.py
+++ prospectio_chatbot/main.py
@@ -1,76 +1,22 @@
-import chainlit as cl
-from config import PostgreSettings
-from settings.chat_settings import ChatSettings
-from profiles.chat_profiles import ChatProfiles
-from core.essentials import CoreEssentials
-from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
-from chainlit.types import ThreadDict
-from mcp import ClientSession
-from langchain_mcp_adapters.tools import load_mcp_tools
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from config import ChainlitSettings
+from api.auth.auth import auth_router
 from chainlit.utils import mount_chainlit
-from api.main import app
 
 
+app = FastAPI()
+settings = ChainlitSettings()
+AUTH_PATH="/rest/v1/{}"
 
-chat_settings = ChatSettings().get_chat_settings()
-chat_profiles = ChatProfiles().get_chat_profiles()
-core = CoreEssentials()
-postgre_settings = PostgreSettings()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(auth_router, prefix=AUTH_PATH.format("auth"), tags=["Auth"])
 
-@cl.on_mcp_connect 
-async def on_mcp(connection, session: ClientSession):
-    result = await load_mcp_tools(session)
-    cl.user_session.set("mcp_tools",result)
-    
-
-@cl.data_layer  
-def get_data_layer():
-    return SQLAlchemyDataLayer(
-        conninfo=postgre_settings.POSTGRE_CONNECTION_STRING, storage_provider=None
-    )
-
-
-@cl.on_chat_resume  
-async def resume_conversation(thread: ThreadDict):
-    settings = thread.get("metadata").get("chat_settings")  # type: ignore
-    await core.setup_chat(settings["Model"], settings["Temperature"])  # type: ignore
-    await cl.ChatSettings(chat_settings).send() 
-
-
-@cl.password_auth_callback   # type: ignore
-def auth_callback(username: str, password: str):
-    if username == "prospectio" and password == "prospectio":
-        return cl.User(
-            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
-        ) 
-    return None
-
-
-@cl.set_chat_profiles   # type: ignore
-async def chat_profile():
-    return chat_profiles
-
-
-@cl.on_chat_start
-async def on_chat_start():
-    await core.setup_chat(chat_settings[0].values[0], chat_settings[1].initial)  # type: ignore
-    await cl.ChatSettings(chat_settings).send() 
-
-
-@cl.on_message  
-async def main(msg: cl.Message): 
-    try:
-        response = await core.call_agent(msg)
-        await core.process_response(response)
-    except Exception as e:
-        await cl.Message(content=f"{type(e).__name__}: {e}").send()  
-
-
-@cl.on_settings_update  
-async def setup_agent(settings):
-    await core.setup_chat(settings["Model"], settings["Temperature"])  
-
-
-if __name__ == "__main__":
-    mount_chainlit(app=app,target=__file__)
+mount_chainlit(app=app, target="prospectio_chatbot/cl_app.py", path="/chainlit")
